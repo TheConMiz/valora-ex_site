4b5339e3e5686bf3911c873e8f0612c3de271096
Vishal Venkatraman <vishal.venkatraman97@gmail.com>
2026-07-07 13:58:03 +0800

    Small UI Amendments

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-// import Image from "next/image";
-
-// export default function Home() {
-//   return (
-//     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-//       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-//       </main>
-//     </div>
-//   );
-// }
 import React from 'react';
 import { ArrowRight, CheckCircle, Mail, MapPin, LogIn } from 'lucide-react';
-
 // ============================================================================
 // 🛠️ DATA ARCHITECTURE 
 // Update these variables with your specific product details.
@@ -149,4 +138,4 @@ export default function LandingPage() {
 
     </div>
   );
-}``
+}
