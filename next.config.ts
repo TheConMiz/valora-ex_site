34da5dc5ce6df310d26ddb4b4d5dfde6d98b1801
Vishal Venkatraman <vishal.venkatraman97@gmail.com>
2026-08-11 17:48:17 +0800

    Undo Dockerfile setup

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'standalone', // Forces Next.js to output static HTML/CSS/JS
 	images: {
 		unoptimized: true, // Required because GitHub Pages won't support Next.js built-in image optimization API
 	},
